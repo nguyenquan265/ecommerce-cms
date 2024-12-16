a6d4428be21d084dfa185e15017f14073c74f485
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-12-16 15:54:41 +0700

    accept all cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,12 +7,7 @@ import router from './routes'
 
 const app = express()
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-  })
-)
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
