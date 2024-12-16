e536202c605b2a29e8e17cb2a1785048a40e7c63
kimochiee <90102971+kimochiee@users.noreply.github.com>
2024-12-16 18:20:25 +0700

    push

--- store_frontend/src/pages/Home.tsx
+++ store_frontend/src/pages/Home.tsx
@@ -11,8 +11,8 @@ const HomePage = () => {
 
   if (isBillbordLoading || isProductsLoading) {
     return (
-      <Container className='min-h-screen'>
-        <div className='flex h-full w-full items-center justify-center'>
+      <Container>
+        <div className='flex h-screen w-full items-center justify-center'>
           <div className='flex flex-col items-center space-y-4'>
             <div className='animate-spin rounded-full border-4 border-gray-300 border-t-gray-900 h-12 w-12' />
             <p className='text-gray-500 dark:text-gray-400'>Loading...</p>
